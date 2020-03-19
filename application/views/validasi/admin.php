cefaa2fa4f03f042a05752efce9c2e51fd5e3cd2
yuslanseptian <septianyuslan26@gmail.com>
2020-03-19 15:26:52 +0700

    Initial commit

---
+++ application/views/validasi/admin.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pendaftaran Mahasiswa</title>
+</head>
+<center>
+<body>
+    <h1>Hai, <?php echo $this->session->userdata('username'); ?></h1>
+	<a href="<?php echo site_url('Cform/tambah'); ?>"> Tambah Data </a> || <a href="<?php echo site_url('Cform/view'); ?>"> Edit delete Data </a><br><br>
+    <a href="<?php echo site_url('Cform/logout'); ?>"> Logout </a>
+</body>
+</center>
+</html>
